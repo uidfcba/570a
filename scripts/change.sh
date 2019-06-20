b9ddfdb0be3fafbcf7a773313f34f72216d1f467
Robert J. Brunner <rb@Roberts-MacBook-Pro-2.local>
2019-06-20 17:13:09 -0500

    Created new HTML files
    Fixed change.sh script and remade all html from new modified content.

---
+++ scripts/change.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Assume notebooks have already been copied into html directory
+# and there are eight modules
+# Convert ipynb extensions in URLs to html
+
+for i in {1..8} ; 
+do 
+    cd Module$i ; 
+    sed -i '' 's/.ipynb/.html/g' index.html
+    cd notebooks ; 
+    for file in m?l?.html ; 
+    do 
+        sed -i '' 's/.ipynb/.html/g' $file ; 
+    done ; 
+    cd ../.. ; 
+done
+
